d73f2afab3263457f53a59023d64d3ee10f43b1d
Atiya <atiyaali653@gmail.com>
2025-10-16 20:13:13 +0800

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+abstract class Controller
+{
+    function __construct()
+    {
+        // This constructor can be used to initialize common properties or middleware
+    }
+    function index(){
+        //this method is for testing purposes
+    }
+}
